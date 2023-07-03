fb2f125a76e991adc84ec0d7d2f33ebaafb4fd3a
정유찬 <81511939+uchan21@users.noreply.github.com>
2023-07-03 21:32:41 +0900

    Add files via upload

---
+++ TREE/Example_of_Book/node.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <queue>
+
+class node
+{
+public:
+	std::string position;
+	node* first;
+	node* second;
+};
+
---
+++ TREE/Example_of_Book/org_tree.h
@@ -0,0 +1,74 @@
+#pragma once
+#include "node.cpp"
+
+class org_tree
+{
+public:
+	node* root;
+	static org_tree create_org_structure(const std::string& pos) {
+		org_tree tree;
+		tree.root = new node{ pos,NULL,NULL };
+		return tree;
+	}
+	static node* find(node* root, const std::string& value) {
+		if (root == NULL)
+			return NULL;
+
+		if (root->position == value)
+			return root;
+
+		auto firstFound = org_tree::find(root->first, value);
+		if (firstFound != NULL)
+			return firstFound;
+
+		return org_tree::find(root->second, value);
+	}
+	bool addSubordinate(const std::string& manager, const std::string& subordinate) {
+		auto managerNode = org_tree::find(root, manager);
+
+		if (!managerNode) {
+			std::cout << manager << "을 찾을 수 없습니다: " << std::endl;
+			return false;
+		}
+
+		if (managerNode->first && managerNode->second) {
+			std::cout << manager << " 아래에 " << subordinate << "을 추가할 수 없습니다" << std::endl;
+			return false;
+		}
+
+		if (!managerNode->first)
+			managerNode->first = new node{ subordinate, NULL, NULL };
+		else
+			managerNode->second = new node{ subordinate, NULL,NULL };
+
+		std::cout << manager << " 아래에 " << subordinate << " 을 추가했습니다." << std::endl;
+
+		return true;
+	}
+	static void preOrder(node* start) {//전위 순회
+		if (!start)
+			return;
+		std::cout << start->position << ", ";
+		preOrder(start->first);
+		preOrder(start->second);
+	}
+
+	static void inOrder(node* start) { //중위 순회
+		if (!start)
+			return;
+
+		inOrder(start->first);
+		std::cout << start->position << ", ";
+		inOrder(start->second);
+	}
+
+	static void postOrder(node* start) { //후위 순회
+		if (!start)
+			return;
+
+		postOrder(start->first);
+		postOrder(start->second);
+		std::cout << start->position << ", ";
+	}
+};
+
