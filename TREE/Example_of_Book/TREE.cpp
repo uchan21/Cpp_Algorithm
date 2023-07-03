3417ed6f7dd5417cc8dfd79fe1c5fa9e9492c472
정유찬 <81511939+uchan21@users.noreply.github.com>
2023-07-03 21:42:01 +0900

    Update TREE.cpp

--- TREE/Example_of_Book/TREE.cpp
+++ TREE/Example_of_Book/TREE.cpp
@@ -12,10 +12,20 @@ int main() {
 	tree.addSubordinate("마케팅부장", "홍보팀장");
 	tree.addSubordinate("부사장", "재무부장");
 
+	std::cout << "트리 순회" << std::endl;
+
+	std::cout << "전위순회 :" << std::endl;
 	tree.preOrder(tree.root);
 	std::cout << std::endl;
+
+	std::cout << "중위순회 :" << std::endl;
 	tree.inOrder(tree.root);
 	std::cout << std::endl;
+
+	std::cout << "후위순회 :" << std::endl;
 	tree.postOrder(tree.root);
 	std::cout << std::endl;
+
+	std::cout << "레벨 순서 순회 :" << std::endl;
+	tree.levelOrder(tree.root);
 }
