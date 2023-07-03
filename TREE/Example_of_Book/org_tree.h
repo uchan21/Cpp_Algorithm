02ce1b5e011eecc8d4588d1dcf0ec9e17ce4cbea
정유찬 <81511939+uchan21@users.noreply.github.com>
2023-07-03 21:42:30 +0900

    Update org_tree.h

--- TREE/Example_of_Book/org_tree.h
+++ TREE/Example_of_Book/org_tree.h
@@ -70,5 +70,27 @@ public:
 		postOrder(start->second);
 		std::cout << start->position << ", ";
 	}
+
+	static void levelOrder(node* start) {
+		if (!start)
+			return;
+		std::queue<node*> q;
+		q.push(start);
+
+		while (!q.empty()) {
+			int size = q.size();
+			for (int i = 0; i < size; i++) {
+				auto current = q.front();
+				q.pop();
+
+				std::cout << current->position << ", ";
+				if (current->first)
+					q.push(current->first);
+				if (current->second)
+					q.push(current->second);
+			}
+			std::cout << std::endl;
+		}
+	}
 };
 
